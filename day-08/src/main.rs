17753875f75be17a342750682b85c434e124133c
Miah Mohd <miahmohd98@gmail.com>
2023-12-08 13:46:38 +0100

    day 8 done

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, ops::Deref};
+use std::collections::{BTreeMap, HashSet};
 
 fn main() {
     let res = part1(include_str!("./input"));
@@ -10,10 +10,10 @@ fn main() {
     // println!("Part 2: {res}");
 }
 
-fn part1(s: &str) -> i32 {
+fn part1(s: &str) -> u64 {
     let lines: Vec<&str> = s.lines().collect();
     let insts: Vec<char> = lines[0].chars().collect();
-    let mut map: HashMap<&str, (&str, &str)> = HashMap::new();
+    let mut map: BTreeMap<&str, (&str, &str)> = BTreeMap::new();
 
     for l in lines.iter().skip(2) {
         map.insert(&l[0..3], (&l[7..10], &l[12..15]));
@@ -22,81 +22,99 @@ fn part1(s: &str) -> i32 {
     dbg!(&insts);
     dbg!(&map);
 
-    let mut steps = 0;
-    let mut current_nodes: Vec<&str> = map
+    let current_nodes: Vec<&str> = map
         .keys()
         .filter(|k| k.chars().nth_back(0) == Some('A'))
         .map(|k| *k)
         .collect();
-    let mut next_nodes = Vec::with_capacity(current_nodes.len());
+    let mut last_node_steps = Vec::with_capacity(current_nodes.len());
 
-    loop {
-        let inst = insts[steps % insts.len()];
+    for node in current_nodes {
+        let mut current = node;
+        let mut steps = 0;
+        let mut visited_set = HashSet::new();
+        let mut step_until_last = Vec::new();
+        visited_set.insert((current, steps % insts.len()));
 
-        for current in &current_nodes {
+        loop {
+            let inst = insts[steps % insts.len()];
+
             let next = map.get(current).unwrap();
 
             match inst {
-                'R' => next_nodes.push(next.1),
-                'L' => next_nodes.push(next.0),
+                'R' => current = next.1,
+                'L' => current = next.0,
                 _ => unreachable!(),
             }
+
+            steps += 1;
+
+            // println!(
+            //     "Node: {}, Current: {}, Steps: {}, inst: {}",
+            //     node,
+            //     current,
+            //     steps,
+            //     steps % insts.len()
+            // );
+
+            if current.chars().nth_back(0) == Some('Z') {
+                step_until_last.push((node, current, steps));
+            }
+
+            if visited_set.contains(&(current, steps % insts.len())) {
+                break;
+            }
+
+            visited_set.insert((current, steps % insts.len()));
         }
-
-        steps += 1;
-
-        if steps % 1_000 == 0 {
-            println!("Step: {}", steps);
-            println!("Curr nodes: {:?}", current_nodes);
-            println!("Next nodes: {:?}", next_nodes);
-        }
-
-        if next_nodes
-            .iter()
-            .all(|k| k.chars().nth_back(0) == Some('Z'))
-        {
-            break;
-        }
-
-        current_nodes.clear();
-        current_nodes.append(&mut next_nodes);
+        // println!();
+        last_node_steps.push(step_until_last);
     }
 
-    steps as i32
+    println!("Steps until last: {:?}", last_node_steps);
+
+    let mut acc = 1;
+
+    for l in last_node_steps {
+        let x = l.first().unwrap().2 as u64;
+        acc = num::integer::lcm(acc, x);
+    }
+
+    acc
 }
 
 #[cfg(test)]
 mod tests {
     use crate::part1;
 
-    #[test]
-    fn it_works_part1() {
-        assert_eq!(
-            part1(
-                "RL
+    // #[test]
+    //     fn it_works_part1() {
+    //         assert_eq!(
+    //             part1(
+    //                 "RL
 
-AAA = (BBB, CCC)
-BBB = (DDD, EEE)
-CCC = (ZZZ, GGG)
-DDD = (DDD, DDD)
-EEE = (EEE, EEE)
-GGG = (GGG, GGG)
-ZZZ = (ZZZ, ZZZ)"
-            ),
-            2
-        );
+    // AAA = (BBB, CCC)
+    // BBB = (DDD, EEE)
+    // CCC = (ZZZ, GGG)
+    // DDD = (DDD, DDD)
+    // EEE = (EEE, EEE)
+    // GGG = (GGG, GGG)
+    // ZZZ = (ZZZ, ZZZ)"
+    //             ),
+    //             2
+    //         );
 
-        assert_eq!(
-            part1(
-                "LLR
+    //         assert_eq!(
+    //             part1(
+    //                 "LLR
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)"
-            ),
-            6
-        );
-    }
+    // AAA = (BBB, BBB)
+    // BBB = (AAA, ZZZ)
+    // ZZZ = (ZZZ, ZZZ)"
+    //             ),
+    //             6
+    //         );
+    //     }
 
     #[test]
     fn it_works_part2() {
@@ -113,7 +131,7 @@ ZZZ = (ZZZ, ZZZ)"
 22Z = (22B, 22B)
 XXX = (XXX, XXX)"
             ),
-            6
+            7
         );
     }
 }
